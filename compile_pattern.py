4f62f7d32eb2f76193b5265e28ef14804f53a2d7
hjbyt <sopu00@gmail.com>
2016-05-15 21:50:06 +0300

    added offset options to compile_pattern.py

--- compile_pattern.py
+++ compile_pattern.py
@@ -3,7 +3,7 @@ import argparse
 
 def main():
     args = parse_args()
-    compile_pattern(args.input, args.output, args.n)
+    compile_pattern(args.input, args.output, args.n, args.x, args.y)
 
 
 def parse_args():
@@ -11,6 +11,8 @@ def parse_args():
     parser.add_argument('input', type=argparse.FileType('r'), help='input .cells file')
     parser.add_argument('output', type=argparse.FileType('wb'), help='output file')
     parser.add_argument('n', type=int, help='dimension of output file (power of 2)')
+    parser.add_argument('-x', type=int, default=0, help='offset in x dimension')
+    parser.add_argument('-y', type=int, default=0, help='offset in y dimension')
     args = parser.parse_args()
     if not is_power2(args.n):
         parser.error('n should be a power of 2')
@@ -21,9 +23,13 @@ def is_power2(num):
     return num != 0 and ((num & (num - 1)) == 0)
 
 
-def compile_pattern(input, output, n):
+def compile_pattern(input, output, n, offset_x=0, offset_y=0):
     input_lines = input.read().splitlines()
     input_lines = [line for line in input_lines if not line.startswith('!') and line.strip() != '']
+    # add offsets
+    input_lines = ['.' * offset_x + line for line in input_lines]
+    input_lines = [''] * offset_y + input_lines
+    
     for x in range(n):
         for y in range(n):
             try:
